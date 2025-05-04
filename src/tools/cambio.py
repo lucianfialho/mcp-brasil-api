cb3fdded32e05732827085864d01c1ac6dffd58e
Raphael Loureiro <rloureiro.92@gmail.com>
2025-05-04 17:46:39 -0300

    Subida de Branch

--- src/tools/cambio.py
+++ src/tools/cambio.py
@@ -1,5 +1,5 @@
 """
-Ferramenta para consulta de cambio
+Ferramenta para consulta de câmbio
 """
 
 from typing import Dict, Any
@@ -8,26 +8,24 @@ from ..utils.formatters import format_data
 
 async def get_lista_cambio() -> Dict[str, Any]:
     """
-    Obtém informações de Cambio.
+    Obtém informações de Câmbio.
        
     Returns:
-        Um dicionário contendo informações relacionadas a moedas de cambio.
+        Um dicionário contendo informações relacionadas a moedas de câmbio.
     """
-         
-    return await make_request("lista_cambio","")
+    return await make_request("lista_cambio", "")
 
-async def get_cambio_info(moeda:str,data:str) -> Dict[str, Any]:
+async def get_cambio_info(moeda: str, data: str) -> Dict[str, Any]:
     """
-    Obtém informações de Cambio comparado ao real.
+    Obtém informações de Câmbio comparado ao real.
 
     Args:
-        moeda (str): A Moeda será obtida através do valor do simbolo na pesquisa get_lista_cambio . Exemplo: "USD", "EUR", etc.
+        moeda (str): A Moeda será obtida através do valor do símbolo na pesquisa get_lista_cambio.
+                   Exemplo: "USD", "EUR", etc.
         data (str): A data será obtida através do input do cliente.
 
     Returns:
-        List[Dict[str, Any]]: lista de valores de paridade de moedas com o real brasileiro.
+        Dict[str, Any]: Dicionário contendo a cotação da moeda na data especificada.
     """
-
     formatted_data = format_data(data)
-         
-    return await make_request("cambio",moeda,formatted_data)
+    return await make_request("cambio", moeda, formatted_data)
