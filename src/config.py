e083c628f46b2f1df94a780178e46814b3b1b1f8
Lucian Fialho <lucianfialhobp@hotmail.com>
2025-05-04 15:20:28 -0300

    -- movendo para estrutura inicial

---
+++ src/config.py
@@ -0,0 +1,8 @@
+API_BASE_URL = "https://brasilapi.com.br/api"
+USER_AGENT = "brasil-api/1.0"
+
+API_PATHS = {
+    "cep": "/cep/v2/",
+    "cnpj": "/cnpj/v1/",
+    # Add more API paths as needed
+}
