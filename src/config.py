ce9705189d763f33b407ba9fc0cfa0e383ecb963
Raphael Loureiro <rloureiro.92@gmail.com>
2025-05-04 21:43:28 -0300

    fix no path lista_cambio

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ USER_AGENT = "brasil-api/1.0"
 API_PATHS = {
     "cep": "/cep/v2/",
     "cnpj": "/cnpj/v1/",
-    "lista_cambio": "/cambio/v1/moedas/",
+    "lista_cambio": "/cambio/v1/moedas",
     "cambio": "/cambio/v1/cotacao/",
     "ddd": "/ddd/v1/",
     "lista_banco": "/banks/v1",
